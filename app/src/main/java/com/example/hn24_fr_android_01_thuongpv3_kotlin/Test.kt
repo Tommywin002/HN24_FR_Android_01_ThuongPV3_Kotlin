e055928310de91f297d7ec1570a66c1379eeaef4
Tom <vanthuong00002@gmail.com>
2024-03-14 14:32:11 +0700

    up demo file

--- app/src/main/java/com/example/hn24_fr_android_01_thuongpv3_kotlin/Test.kt
+++ app/src/main/java/com/example/hn24_fr_android_01_thuongpv3_kotlin/Test.kt
@@ -1,7 +1,12 @@
 package com.example.hn24_fr_android_01_thuongpv3_kotlin
 
 fun main(){
-    var name: String? = "hello"
-    var lengthOfName = name?.length
-    println("length = $lengthOfName")
+    var a = 1
+    var b = 2
+    val max = if(a > b){
+        a + b
+    } else{
+        a - b
+    }
+    println(max)
 }
