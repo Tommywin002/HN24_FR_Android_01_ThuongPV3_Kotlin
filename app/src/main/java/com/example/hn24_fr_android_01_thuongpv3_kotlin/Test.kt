727667c662d073521c7a03526928b64c54b9e46e
Tom <vanthuong00002@gmail.com>
2024-03-14 14:14:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/hn24_fr_android_01_thuongpv3_kotlin/Test.kt
@@ -0,0 +1,7 @@
+package com.example.hn24_fr_android_01_thuongpv3_kotlin
+
+fun main(){
+    var name: String? = "hello"
+    var lengthOfName = name?.length
+    println("length = $lengthOfName")
+}
