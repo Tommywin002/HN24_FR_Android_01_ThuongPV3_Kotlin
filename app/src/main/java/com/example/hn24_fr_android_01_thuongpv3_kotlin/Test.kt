e8502abe068a57f86cace83a65192cef6ad8bc9c
Tom <vanthuong00002@gmail.com>
2024-03-14 14:38:00 +0700

    up demo file

--- app/src/main/java/com/example/hn24_fr_android_01_thuongpv3_kotlin/Test.kt
+++ app/src/main/java/com/example/hn24_fr_android_01_thuongpv3_kotlin/Test.kt
@@ -1,10 +1,12 @@
 package com.example.hn24_fr_android_01_thuongpv3_kotlin
 
 fun main(){
-    var a = 1
+    var a = 3
     var b = 2
+    var c = 0
     val max = if(a > b){
-        a + b
+        c = a + b
+        c
     } else{
         a - b
     }
